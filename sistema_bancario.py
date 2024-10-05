eb4e7c14cd6f36e62d980f60560dce73d0648fca
let_say <sayuri.faria@unifesp.br>
2024-10-04 21:21:21 -0300

    desafio do sistema bancário

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,12 +1,11 @@
-#criar sistema bancário p
 saldo = 0
 extrato = ""
 numero_de_saques = 0
 valor_limite = 500
 deposito = 0
 LIMITE_SAQUES = 3
-extrato_mensagem = "EXTRATO"
-espaco = ""
+EXTRATO_MENSAGEM = "EXTRATO"
+ESPACO = ""
 
 menu = """
  ------------------------
@@ -19,10 +18,10 @@ menu = """
 """
 
 def visualizar_extrato():
-    print(extrato_mensagem.center(40, "="))
+    print(EXTRATO_MENSAGEM.center(40, "="))
     print ("Não foram relizadas movimentações" if not extrato else extrato)
     print (f"\nSaldo:    R$ {saldo.center:.2f(40)}")
-    print (espaco.center(40, "="))
+    print (ESPACO.center(40, "="))
 
 def realizar_saque():
     global extrato, saldo, numero_de_saques
