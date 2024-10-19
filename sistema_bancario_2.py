ed52e0a2e9032b6f518e1acf8ffd1f82f8d5bfe9
Leticia Faria <142911045+letsay@users.noreply.github.com>
2024-10-19 17:15:30 -0300

    Update sistema_bancario_2.py

--- sistema_bancario_2.py
+++ sistema_bancario_2.py
@@ -39,17 +39,13 @@ def visualizar_extrato(saldo, /, *, extrato):
 def realizar_saque():
     global extrato, saldo, numero_de_saques
     saque = float(input("Valor que deseja sacar:\n"))
-
     excedeu_saldo = saque > saldo
     excedeu_limite = saque > valor_limite
     excedeu_saques = numero_de_saques >= LIMITE_SAQUES
-
     if excedeu_saldo or excedeu_limite:
         print("Valor de saque inválido!")
-
     elif excedeu_saques:
         print("Operação falhou! Número máximo de saques excedido.")
-
     elif saque > 0:
             saldo -= saque
             extrato += f"Saque:\t\tR${saque:.2f}\n"
@@ -84,47 +80,36 @@ def endereco_func():
     global endereco
     endereco = f"{logradouro},  {nro} - {bairro} - {cidade}/{sigla_estado}"
     
-#TODO incluir verificação de CPF
 def cria_usuario(usuarios):
     cpf = input("Insira seu CPF (somente números): ")
     usuario = filtra_usuario(cpf, usuarios)
-
     if usuario:
         print("Já existe usuário com esse CPF")
         return
-    
     while True:
         p_nome = input("Nome: ")
         if all(caractere.isalpha() for caractere in p_nome):
             break
         else:
             print('Digite somente o primeiro nome.')
-
     while True:
         sobrenome = input("Sobrenome: ")
         if all(caractere.isalpha() for caractere in sobrenome):
             break
         else:
             print('Digite somente o sobrenome.')
-
     nome = p_nome + " " + sobrenome
     data_func() 
     endereco_func()
-
-   
     usuarios.append({'nome': nome, 'data_nascimento': data_nascimento, 'cpf': cpf, 'endereco': endereco})
-    print(usuarios)
     print("Usuário criado com sucesso!")
 
-#TODO fazer com que o input de cpf aceito seja só de inteiros nome: limpa_cpf talvez
 def criar_conta(AGENCIA, numero_conta, usuarios):
     cpf = input("Informe o cpf do usuário: ")
     usuario = filtra_usuario(cpf, usuarios)
-
     if usuario:
         print("Conta criada com sucesso!")
         return {'agencia': AGENCIA, 'numero_conta': numero_conta, 'usuario': usuario}
-
     print("Usuário não encontrado, fluxo de criação de conta encerrado.")
 
 def listar_contas(contas):
@@ -134,7 +119,6 @@ def listar_contas(contas):
             C/C:\t\t{conta['numero_conta']}
             Titular:\t{conta['usuario']['nome']}
         """
-
         print("=" * 100)
         print(linha)
 
@@ -145,30 +129,22 @@ while True:
         visualizar_extrato(saldo, extrato = extrato)
     elif opcao == 2:
         realizar_saque()
-
     elif opcao == 3:
         deposito = float(input("Qual o valor de depósito?\n"))
         deposito_funcoes['print_deposito'](deposito) 
         if deposito > 0:
             saldo = deposito_funcoes['atualiza_saldo'](deposito)  
             extrato += deposito_funcoes['add_extrato'](deposito)
-
     elif opcao == 4:
         cria_usuario(usuarios)
-
     elif opcao == 5:
-       # numero_conta = len(contas) + 1
         conta = criar_conta(AGENCIA, numero_conta, usuarios)
-
         if conta: 
             contas.append(conta)
             numero_conta += 1
-
     elif opcao == 6:
         listar_contas(contas)
-
     elif opcao == 7:
         break
-    
     else:
         print("Opção inválida, por favor selecione novamente a opção desejada.")
